9faf918c235223d78be95487b02b7cddbab22608
Etienne CALAIS <etienne75012@gmail.com>
2025-03-13 00:28:16 +0100

    formation jour 2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-let a=false;
-let b=false;
-
-
-    console.log(undefined == false)
+(12312).toExponential(); // "1.2312e+4"
+(12312).toExponential(2); // "1.23e+4"
+const a = 100000000;
+a.toExponential(); // "1e+8"
