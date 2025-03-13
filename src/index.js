a9807a6494b23ff1715f7e264d73170cdbfa004f
Etienne CALAIS <etienne75012@gmail.com>
2025-03-13 21:19:02 +0100

    jour3

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
-(12312).toExponential(); // "1.2312e+4"
-(12312).toExponential(2); // "1.23e+4"
-const a = 100000000;
-a.toExponential(); // "1e+8"
+const obj = {
+    firstname:'Jean',
+    lastname:'Louis',
+    age:15
+}
+console.log(JSON.stringify(obj));
+
+console.log(JSON.parse(`{"firstname":"Jean","lastname":"Louis","age":"15"}`))
