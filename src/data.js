8407ce8cffa8c49200677ee9ce09ee17a4af15fd
Etienne CALAIS <etienne75012@gmail.com>
2025-03-23 11:50:33 +0100

    projet_avance

---
+++ src/data.js
@@ -0,0 +1,310 @@
+class task{
+    constructor(name){
+        this.name=name;
+        this.isEdit=false;
+    }
+    static name;
+    static isEdit;
+}
+class taskDay{
+    constructor(tache){
+        if (tache.jour) this.jour=tache.jour;
+        if (tache.toDo) {this.toDo=tache.toDo} else {this.toDo=[]};
+        if (tache.isDone) {this.isDone=tache.isDone}else {this.isDone=[]};
+        if (tache.isDelete) {this.isDelete=tache.isDelete}else {this.isDelete=[]};
+    }
+    static day;
+    static toDo=[];
+    static isDone=[];
+    static isDelete=[];
+    addToDo(tache){
+        const tacheN=new task(tache);
+        this.toDo.push(tacheN)
+    }
+    getToDoAll(){
+        return this.toDo;
+    }
+    getToDoTache(tache){
+        for (let i = 0;i<this.toDo.length;i++){
+            if (this.toDo[i].name===tache){
+                return this.toDo[i]
+            }
+        }
+        return false;
+    }
+    delToDo(tache){
+        for (let i = 0;i<this.toDo.length;i++){
+            if (this.toDo[i].name===tache){
+                this.toDo.splice(i,1)
+            }
+        }
+    }
+    addIsDone(tache){
+        const tacheN=new task(tache);
+        this.isDone.push(tacheN)
+    }
+    getIsDoneAll(){
+        return this.isDone;
+    }
+    getIsDoneTache(tache){
+        for (let i = 0;i<this.isDone.length;i++){
+            if (this.isDone[i].name===tache){
+                return this.isDone[i]
+            }
+        }
+        return false;
+    }
+    delIsDone(tache){
+        for (let i = 0;i<this.isDone.length;i++){
+            if (this.isDone[i].name===tache){
+                this.isDone.splice(i,1)
+            }
+        }
+    }
+    addIsDelete(tache){
+        const tacheN=new task(tache);
+        this.isDelete.push(tacheN)
+    }
+    getIsDeleteAll(){
+        return this.isDelete;
+    }
+    getIsDeleteTache(tache){
+        for (let i = 0;i<this.isDelete.length;i++){
+            if (this.isDelete[i].name===tache){
+                return this.isDelete[i]
+            }
+        }
+        return false;
+    }
+    delIsDelete(tache){
+        for (let i = 0;i<this.isDelete.length;i++){
+            if (this.isDelete[i].name===tache){
+                this.isDelete.splice(i,1)
+            }
+        }
+    }
+}
+class tasksWeek{
+    constructor(tasks){
+        if (tasks.monday)  {
+            const tache=new taskDay(tasks.monday)
+            this.monday=tache;}
+         if (tasks.tuesday)  {
+            const tache=new taskDay(tasks.tuesday)
+            this.tuesday=tache;}
+        if (tasks.wednesday)  {
+            const tache=new taskDay(tasks.wednesday)
+            this.wednesday=tache;}
+        if (tasks.thursday)  {
+            const tache=new taskDay(tasks.thursday)
+            this.thursday=tache;}
+        if (tasks.friday)  {
+            const tache=new taskDay(tasks.friday)
+            this.friday=tache;}
+        if (tasks.saturday)  {
+            const tache=new taskDay(tasks.saturday)
+            this.saturday=tache;}        
+        if (tasks.sunday)  {
+            const tache=new taskDay(tasks.sunday)
+            this.sunday=tache;}
+    }
+    static monday;
+    static tuesday;
+    static wednesday;
+    static thursday;
+    static friday;
+    static saturday;
+    static sunday;
+    addTaskDay(tachejournee){
+        if (tachejournee.day){
+            switch (tachejournee.day){
+                case "Lundi":
+                this.monday=tachejournee;
+                return true;    
+                case "Mardi":
+                this.tuesday=tachejournee;
+                return true;    
+                case "Mercredi":
+                this.wednesday=tachejournee;
+                return true;    
+                case "Jeudi":
+                this.thursday=tachejournee;
+                return true;    
+                case "Vendredi":
+                this.friday=tachejournee;
+                return true;    
+                case "Samedi":
+                this.saturday=tachejournee;
+                return true;    
+                case "Dimanche":
+                this.sunday=tachejournee;
+                return true;    
+                default:
+                return false;}
+        }
+        else{
+            return false;
+        }
+    }
+    getTaskDay(journee){
+            switch (journee){
+            case "Lundi":
+                return this.monday;   
+            case "Mardi":
+                return this.tuesday;  
+            case "Mercredi":
+                return this.wednesday;   
+            case "Jeudi":
+                return this.thursday;
+            case "Vendredi":
+                return this.friday;
+            case "Samedi":
+                    return his.saturday;
+            case "Dimanche":
+                return this.sunday;
+            default:
+            return false;}
+    }
+      
+    
+    delTaskDay(tachejournee){
+        if (tachejournee.day){
+            switch (tachejournee.day){
+                case "Lundi":
+                    this.monday=null;
+                    return true;   
+                case "Mardi":
+                    this.tuesday=null;
+                    return true;  
+                case "Mercredi":
+                    this.wednesday=null;
+                    return true;   
+                case "Jeudi":
+                    this.thursday=null;
+                    return true;
+                case "Vendredi":
+                    this.friday=null;
+                    return true;
+                case "Samedi":
+                    this.saturday=null;
+                    return true;
+                case "Dimanche":
+                    this.sunday=null;
+                    return true;
+                default:
+                return false;}
+        }
+        else{
+            return false;
+        }
+    }
+
+}
+class user{
+    static login;
+    static tachesSemaine;
+    constructor(personne){
+        if (personne.login){
+            this.login=personne.login;
+            if (personne.tachesSemaine){
+            this.tachesSemaine=new tasksWeek(personne.tachesSemaine);}
+        }
+    };
+    match(utilisateur){
+        return utilisateur ===this.login;
+    }
+    addTaskDay(tachejournee){
+        return this.tachesSemaine.addTaskDay(tachejournee);
+    }
+    getTaskDay(tachejournee){
+        return this.tachesSemaine.getTaskDay(tachejournee);
+    }
+    delTaskDay(tachejournee){
+        return this.tachesSemaine.delTaskDay(tachejournee);
+    }
+}
+class data{
+    static info;
+    
+    constructor(){
+        this.info={utilisateurs:[]};console.log("constructeur");
+    };
+
+    getInfoG()  {
+        return this.info;       
+    };
+    //Gestion personne
+    addPersonne(personne){
+        const utilisateur = new user(personne);
+        this.info.utilisateurs.push(utilisateur);
+        return utilisateur
+    }
+    getPersonne(personne){
+        console.log("getpersonne"+personne)
+        for(let i =0;i<this.info.utilisateurs.length;i++){
+            if (this.info.utilisateurs[i].match(personne)){return this.info.utilisateurs[i]}
+        }       
+        return false;
+    };
+    delPersonne(personne){
+        for(let i =0;i<this.info.utilisateurs.length;i++){
+            if (this.info.utilisateurs[i].match(personne)){this.info.utilisateurs.splice(i,1)}
+        }       
+        return false;
+    };
+
+    //Gestion taches de la semaine
+    addTaskDay(personne,tachejournee){
+        return this.getPersonne(personne).addTaskDay(tachejournee)
+    }
+    getTaskDay(personne,journee){
+        return this.getPersonne(personne).getTaskDay(journee)
+    };
+    delTaskDay(personne,tachejournee){
+        return this.getPersonne(personne).delTaskDay(tachejournee)
+    };
+
+    //Gestion taches toDo
+    addTaskDayToDo(personne,jour,tache){
+        return this.getTaskDay(personne,jour).addToDo(tache);
+    }
+    getaskDayToDo(personne,jour){
+        return this.getTaskDay(personne,jour).getToDoAll();
+    }
+    getToDo(personne,jour,tache){
+        return this.getTaskDay(personne,jour).getToDoTache(tache);
+    }
+    delTaskDayToDo(personne,jour,tache){
+        return this.getTaskDay(personne,jour).delToDo(tache);
+    } 
+    //Gestion taches isDone
+    addTaskDayIsDone(personne,jour,tache){
+        return this.getTaskDay(personne,jour).addIsDone(tache);
+    }
+    getaskDayIsDone(personne,jour){
+        return this.getTaskDay(personne,jour).getIsDoneAll();
+    }
+    getIsDone(personne,jour,tache){
+        return this.getTaskDay(personne,jour).getIsDoneTache(tache);
+    }
+    delTaskDayIsDone(personne,jour,tache){
+        return this.getTaskDay(personne,jour).delIsDone(tache);
+    }   
+    //Gestion taches isDelete
+    addTaskDayIsDelete(personne,jour,tache){
+        return this.getTaskDay(personne,jour).addIsDelete(tache);
+    }
+    getaskDayIsDelete(personne,jour){
+        return this.getTaskDay(personne,jour).getIsDeleteAll();
+    }
+    getIsDelete(personne,jour,tache){
+        return this.getTaskDay(personne,jour).getIsDeleteTache(tache);
+    }
+    delTaskDayIsDelete(personne,jour,tache){
+        return this.getTaskDay(personne,jour).delIsDelete(tache);
+    }    
+};
+var dataW = new data();
+export function getData(){
+    return(dataW);
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
 import "./style.css";
+import * as data from "/data.js";
+let donnees =data.getData();
+if (localStorage.getItem("TODODATA")){
+    let recup=JSON.parse(localStorage.getItem("TODODATA"))
+    for (let i=0;i<recup.info.utilisateurs.length;i++) {
+        donnees.addPersonne(recup.info.utilisateurs[i])
+    }
+}
+else{
+    donnees.addPersonne({login:"Etienne",tachesSemaine:{monday:{jour:"lundi"}}})
+}
+donnees =data.getData();
 
-const ul = document.querySelector("ul");
-const form =document.querySelector("form");
-const input=document.querySelector("form>input");
+const ul = document.getElementById("ul1");
+const form =document.getElementById("form1");
+const input=document.getElementById("input1");
 
+const ul2 = document.getElementById("ul2");
+const ul3 = document.getElementById("ul3");
+
+
+const todos =donnees.getaskDayToDo("Etienne","Lundi");
+const isdones =donnees.getaskDayIsDone("Etienne","Lundi");
+const isdelteted =donnees.getaskDayIsDelete("Etienne","Lundi");
 form.addEventListener("submit",(event)=>{
     event.preventDefault();
     const value = input.value;
@@ -11,32 +30,28 @@ form.addEventListener("submit",(event)=>{
     addTodo(value);
 })
 
-const todos =[
-    {
-        text:"je suis une todo",
-        done:false,
-        editMode:false
-    },
-    {
-        text:"faire du JavaScript",
-        done:true,
-        editMode:true
-    }
-];
-
+const personne="Etienne"
 const displayTodo =()=>{
-    const todosNode = todos.map((todo,index)=> {
-        if(todo.editMode){
-            return createTodoEditElement(todo,index);
-        } else{
-            return createTodoElement(todo,index);
-        }
-    })
     ul.innerHTML="";
-    ul.append(...todosNode)    
+   for (let index=0;index<todos.length;index++){
+        if(todos[index].isEdit){
+            ul.append(createTodoEditElement(todos[index].name));
+        } else{
+            console.log("ul");
+            ul.append(createTodoElement(todos[index].name));
+        }
+    }
+    ul2.innerHTML="";
+   for (let index=0;index<isdones.length;index++){
+        ul2.append(createIsDoneElement(isdones[index].name));
+    }   
+    ul3.innerHTML="";
+   for (let index=0;index<isdelteted.length;index++){
+        ul3.append(createIsDeleteElement(isdelteted[index].name));
+    }  
 };
 
-const createTodoElement=(todo,index)=>{
+const createTodoElement=(todo)=>{
     const li =document.createElement("li");
     const buttonDelete=document.createElement("button");
     const buttonEdit = document.createElement("button");
@@ -44,69 +59,113 @@ const createTodoElement=(todo,index)=>{
     buttonEdit.innerHTML="Edit";
     buttonDelete.addEventListener("click",(event)=>{
         event.stopPropagation();
-        deleteTodo(index);
+        deleteTodo(todo);
     })
     buttonEdit.addEventListener("click",(event)=>{
         event.stopPropagation();
-        toggleEditMode(index);
+        toggleEditMode(todo);
     })
     li.innerHTML=`
-    <span class="todo ${todo.done ? "done" : ""}"></span>
-    <p>${todo.text}</p>
-    `;
+    <span class="todo"></span>
+    <p>${todo}</p>
+    `;    
     li.addEventListener("click",(event)=>{
-        toggleTodo(index);
+        toggleTodo(todo);
     })
     li.append(buttonEdit,buttonDelete);
+    li.className="ligne";
     return li;
 };
-const createTodoEditElement=(todo,index)=>{
+const createTodoEditElement=(todo)=>{
     const li =document.createElement("li");
     const input = document.createElement("input");
     const buttonSave=document.createElement("button");
     const buttonCancel=document.createElement("button");
     buttonCancel.addEventListener("click",(event)=>{
         event.stopPropagation();
-        toggleEditMode(index);
+        toggleEditMode(todo);
     })
     buttonSave.addEventListener("click",(event)=>{
         event.stopPropagation();
-        editTodo(index,input);
+        editTodo(todo,input);
     })
     input.type="text";
-    input.value=todo.text;
+    input.value=todo;
     buttonSave.innerHTML="Save";
     buttonCancel.innerHTML="Cancel";
     li.append(input,buttonCancel,buttonSave);
+    li.className="ligneEdit";
+    return li;
+};
+const createIsDoneElement=(isdone)=>{
+    const li =document.createElement("li");
+    li.innerHTML=`
+    <span class="todo done"></span>
+    <p>${isdone}</p>
+    `;
+    li.className="ligne";
+    li.addEventListener("click",(event)=>{
+        toggleIsDone(isdone);
+    })
+    return li;
+};
+const createIsDeleteElement=(isdelete)=>{
+    const li =document.createElement("li");
+    li.innerHTML=`
+    <span class="todo done"></span>
+    <p>${isdelete}</p>
+    `;
+    li.className="ligne";
+    const buttonRestore=document.createElement("button");
+    buttonRestore.addEventListener("click",(event)=>{
+        event.stopPropagation();
+        toggleRestoreMode(isdelete);
+    })
+    buttonRestore.innerHTML="Restore";
+    li.append(buttonRestore)
     return li;
 };
 const addTodo=(text)=>{
-    todos.push({
-        text,
-        done:false
-    })
+    donnees.addTaskDayToDo("Etienne","Lundi",text);
     displayTodo();
 }
 
-const  deleteTodo =(index)=>{
-    todos.splice(index,1);
+const  deleteTodo =(todo)=>{
+    donnees.delTaskDayToDo("Etienne","Lundi",todo);
+    donnees.addTaskDayIsDelete("Etienne","Lundi",todo);
+    displayTodo();
+}
+const toggleRestoreMode=(text)=>{
+    donnees.addTaskDayToDo("Etienne","Lundi",text);
+    donnees.delTaskDayIsDelete("Etienne","Lundi",text);
+    displayTodo();
+}
+const toggleTodo=(text)=>{
+    donnees.addTaskDayIsDone("Etienne","Lundi",text);
+    donnees.delTaskDayToDo("Etienne","Lundi",text);
+    displayTodo();
+}
+const toggleIsDone=(text)=>{
+    donnees.delTaskDayIsDone("Etienne","Lundi",text);
+    donnees.addTaskDayToDo("Etienne","Lundi",text);
+    displayTodo();
+}
+const toggleEditMode=(text)=>{
+    console.log(personne)
+    donnees.getToDo("Etienne","Lundi",text).isEdit=!donnees.getToDo("Etienne","Lundi",text).isEdit
+
     displayTodo();
 }
 
-const toggleTodo=(index)=>{
-    todos[index].done=!todos[index].done
-    displayTodo();
-}
-
-const toggleEditMode=(index)=>{
-    todos[index].editMode=!todos[index].editMode
-    displayTodo();
-}
-
-const editTodo=(index,input)=>{
+const editTodo=(text,input)=>{
     const value=input.value;
-    todos[index].text=value;
-    todos[index].editMode=!todos[index].editMode
+    donnees.getToDo("Etienne","Lundi",text).name=value;
+    donnees.getToDo("Etienne","Lundi",value).isEdit=!donnees.getToDo("Etienne","Lundi",value).isEdit
     displayTodo();
 }
 displayTodo();
+
+
+let sauvegarde =setInterval (()=>{
+    const jsonS=JSON.stringify(data.getData())
+   localStorage.setItem("TODODATA",jsonS)},2000)
